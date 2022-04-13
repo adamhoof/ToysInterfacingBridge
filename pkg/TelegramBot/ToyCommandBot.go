e4abca84bc16560fcbcab07150a9d4cba3261304
adamhoof <adam.hofman1@gmail.com>
2022-04-13 09:17:59 +0200

    add SendTextMessage function in TelegramBot, fix user not initialising field value

--- pkg/TelegramBot/ToyCommandBot.go
+++ pkg/TelegramBot/ToyCommandBot.go
@@ -9,14 +9,14 @@ import (
 )
 
 type ToyCommandBot struct {
-	bot *tb.Bot
+	Bot *tb.Bot
 }
 
 func (toyCommandBot *ToyCommandBot) SetToken(pathToToken string) {
 
 	token, err := ioutil.ReadFile(pathToToken)
 	formattedToken := strings.Split(string(token), "\n")
-	toyCommandBot.bot, err = tb.NewBot(tb.Settings{
+	toyCommandBot.Bot, err = tb.NewBot(tb.Settings{
 		Token: formattedToken[0],
 		Poller: &tb.LongPoller{
 			Timeout: 10 * time.Second,
@@ -25,9 +25,16 @@ func (toyCommandBot *ToyCommandBot) SetToken(pathToToken string) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("bot created")
+	fmt.Println("Bot token valid")
 }
 
 func (toyCommandBot *ToyCommandBot) StartBot() {
-	toyCommandBot.bot.Start()
+	toyCommandBot.Bot.Start()
+}
+
+func (toyCommandBot *ToyCommandBot) SendTextMessage(user User, message string) {
+	_, err := toyCommandBot.Bot.Send(user, message)
+	if err != nil {
+		return
+	}
 }
